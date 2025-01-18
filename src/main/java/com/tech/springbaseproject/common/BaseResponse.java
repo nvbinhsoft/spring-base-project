99a788aae128da208e8ec0417bb2cc02052a2d64
Binh Nguyen Van <nvbinhsoft@gmail.com>
2025-01-19 03:08:06 +0700

    feat: create base response

--- src/main/java/com/tech/springbaseproject/common/BaseResponse.java
+++ src/main/java/com/tech/springbaseproject/common/BaseResponse.java
@@ -1,5 +1,21 @@
 package com.tech.springbaseproject.common;
 
-public class BaseResponse {
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BaseResponse<T> {
+    private int code;
+    private String message;
+    private T data;
+
+    public BaseResponse() {}
+
+    public BaseResponse(int code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
 
 }
